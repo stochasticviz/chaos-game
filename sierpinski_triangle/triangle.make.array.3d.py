5a61374715f4dbd7615181a7dc8ca95a53e19cb6
Ethan Herdrick <herdrick@gmail.com>
2022-10-07 20:53:12 -1000

    sierpinski_triangle: better 3D version

--- sierpinski_triangle/triangle.make.array.3d.py
+++ sierpinski_triangle/triangle.make.array.3d.py
@@ -32,33 +32,47 @@ def y(X):
 def z(X):
     return X[2]
 
-i = 0
 size = (1500, 1500, 1500)
 
-targets = (np.array([0, 0, 0]), np.array([x(size), 0, 0]), np.array([0, y(size), 0]), np.array([x(size), y(size), 0]), np.array([x(size)/2, y(size)/2, z(size)]))
+targets = (np.array([0, 0, 0]), np.array([size[0], 0, 0]), np.array([0, size[1], 0]), np.array([size[0], size[1], 0]), np.array([size[0]/2, size[1]/2, size[2]]))
 
 point = np.array((100,100,100))
 
-X = []
-Y = []
-Z = []
+#    if X_numpy_array is None:
+#        X_numpy_array = np.array(X)
+#    if Y_numpy_array is None:
+#        Y_numpy_array = np.array(Y) #.astype(float)
+#    if Z_numpy_array is None:
+#        Z_numpy_array = np.array(Z)
 
-while i < 100000:
-     target = random.choice(targets)
-     point = (point + target) / 2
-     i = i + 1
-     X.append(point[0])
-     Y.append(point[1])
-     Z.append(point[2])
-
-X = np.array(X).astype(float)
-Y = np.array(Y).astype(float)
-Z = np.array(Z).astype(float)
 
 fig = plt.figure(figsize=(12,10))
 ax = fig.add_subplot(111, projection='3d')
-
-ax.scatter3D(X, Y, Z, s=1, c='black', alpha=.1)
+plt.ion()
 plt.show()
-import time
-time.sleep(5)
+X = []
+Y = []
+Z = []
+i = 0
+POINTS_COUNT_TARGET=100000
+ITERATIONS_BETWEEN_DISPLAY_UPDATES=100000  # try 1000 thru POINTS_COUNT_TARGET
+
+while i < POINTS_COUNT_TARGET:
+    i = i + 1
+    target = random.choice(targets)
+    point = (point + target) / 2
+    X.append(point[0])
+    Y.append(point[1])
+    Z.append(point[2])
+
+    if (i % ITERATIONS_BETWEEN_DISPLAY_UPDATES) == 0:  # this is an opt to update the display less frequently
+        print("i=",i," making numpy arrays. point=",point)
+        ax.scatter3D(np.array(X), np.array(Y), np.array(Z), s=.1, c='black', alpha=.1)  # "s" is the size (diameter) of point
+        plt.draw()
+        plt.pause(.2)
+
+
+    #plt.show()
+
+
+plt.pause(10)
