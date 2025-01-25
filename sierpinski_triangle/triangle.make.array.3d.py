d467a5de346850e46ca16bd701866f7eae5fd28e
Ethan Herdrick <herdrick@gmail.com>
2025-01-24 17:09:25 -1000

    3d: performance opt: do not make unneeded numpy array in main loop

--- sierpinski_triangle/triangle.make.array.3d.py
+++ sierpinski_triangle/triangle.make.array.3d.py
@@ -18,7 +18,7 @@ from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 
 POINTS_COUNT_TARGET=100000
-ITERATIONS_BETWEEN_DISPLAY_UPDATES=1000  # try 1000 thru POINTS_COUNT_TARGET
+ITERATIONS_BETWEEN_DISPLAY_UPDATES=10000  # try 1000 thru POINTS_COUNT_TARGET
 SPACE = (1500, 1500, 1500)
 
 targets = ((0, 0, 0), (SPACE[0], 0, 0), (0, SPACE[1], 0), (SPACE[0], SPACE[1], 0), (SPACE[0]/2, SPACE[1]/2, SPACE[2]))
@@ -30,31 +30,29 @@ plt.show()
 X = []
 Y = []
 Z = []
-i = 0
-point = (100,100,100)
+# arbitrary starting point
+point_x = 100
+point_y = 100
+point_z = 100
 
-while i < POINTS_COUNT_TARGET:
-    i = i + 1
+i = 0
+for i in range(POINTS_COUNT_TARGET):
     target = random.choice(targets)
-    point = numpy.add(point, target) / 2
-    X.append(point[0])
-    Y.append(point[1])
-    Z.append(point[2])
+    point_x = (point_x + target[0]) / 2
+    point_y = (point_y + target[1]) / 2
+    point_z = (point_z + target[2]) / 2
+    X.append(point_x)
+    Y.append(point_y)
+    Z.append(point_z)
     if (i % ITERATIONS_BETWEEN_DISPLAY_UPDATES) == 0:  # this is an opt to update the display less frequently
-        print("points count:",i," making numpy arrays. point=",point)
+        print("points count:", i)
         # apparently ax.scatter3D() works as an appending operation, not a new scatter. So just add the new points.
-        #ax.scatter3D(np.array(X), np.array(Y), np.array(Z), s=1, c='black', alpha=.2, depthshade=True)  # "s" is the size (diameter) of point   #(random.random(), random.random(), random.random()),
         ax.scatter3D(X, Y, Z, s=1, c='black', alpha=.2, depthshade=True)  # "s" is the size (diameter) of point   #(random.random(), random.random(), random.random()),
-        #ax.contour3D(X, Y, Z) # doesn't work, requires X Y Z to all be 2d (?)
-        #ax.plot_trisurf(X, Y, Z, cmap='viridis', edgecolor='none')  # interesting
-        #ax.stem(X, Y, Z)  # not interesting
         plt.draw()
-        plt.pause(.1)
+        plt.pause(.05)
         X = []
         Y = []
         Z = []
-        #if i == 100:
-        #    plt.pause(100)
 
 #plt.pause(10)
-plt.show(block=True)  # on my current backend this pauses until you close the window, and allowing panning and zooming.
+plt.show(block=False)  # on my current backend this pauses until you close the window, and allowing panning and zooming.
