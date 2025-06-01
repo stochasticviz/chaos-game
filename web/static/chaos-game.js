17936912c2cdc51c8e48db3ac129f8a0846343d4
Ethan Herdrick <herdrick@gmail.com>
2025-06-01 11:27:00 -1000

    2D html: in debug mode, manually create a ResultSet object and add entries to it. this is for consistent logging.

--- web/static/chaos-game.js
+++ web/static/chaos-game.js
@@ -228,9 +228,12 @@ function generatePoints(steps, nextVertexAndPointMathJSCodeString, debugMode, co
             } catch (error) { handleMathJSExpressionsError(error); }
         }
         else {
+        // Create a new ResultSet to collect results from individual expressions
+        resultSet = new math.ResultSet();
         for (const [index, expression] of nextVertexAndPointMathJSCodeLines.entries()) {
             try {
-                math.evaluate(expression, scope);
+                const result = math.evaluate(expression, scope);
+                result && resultSet.entries.push(result);
             } catch (error) {
                 const errorDiv = document.getElementById('errorMessage');
                 let highlightedExpression = expression;
