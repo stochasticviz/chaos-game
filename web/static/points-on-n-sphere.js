a6cad377bb55ee651fcadafc272709188caf7258
Ethan Herdrick <herdrick@gmail.com>
2025-11-17 19:01:58 -1000

    comments

--- web/static/points-on-n-sphere.js
+++ web/static/points-on-n-sphere.js
@@ -140,7 +140,7 @@ function getEquidistantPointsOnUnitNSphereApproximation(dimensions, nPoints, opt
 // const pts = getEquidistantPointsOnUnitNSphereApproximation(4, 8, { seed: 42 });
 // console.log(pts.map(v => Array.from(v)));
 
-// TODO: consider changing name of this to getPolytopeOrBestPackedVertices()
+// TODO: consider changing name of this to getPolytopeOrBestPackedVertices() or getPolytopeOrReasonablyPackedVertices()
 export function getEquidistantPointsOnNSphere(dimensions, verticesCount) {
     let vertices = polytopes.regularPolytopeVertices(dimensions, verticesCount);
     if (! vertices)  {
