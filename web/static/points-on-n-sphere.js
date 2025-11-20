9ae314095cf69db616ad0303f0d42690420a3f71
Ethan Herdrick <herdrick@gmail.com>
2025-11-19 20:00:57 -1000

    comments

--- web/static/points-on-n-sphere.js
+++ web/static/points-on-n-sphere.js
@@ -150,7 +150,7 @@ export function getEquidistantPointsOnNSphere(dimensions, verticesCount) {
 
     return vertices.map(vertex => {
           const norm = Math.hypot(...vertex) || 1;
-          return Array.from(vertex).map(coord => coord / norm);
+          return Array.from(vertex).map(coord => coord / norm);  // MathJS.matrix() constructor doesn't like Float64Array, so convert to JS array
       });
 
 
